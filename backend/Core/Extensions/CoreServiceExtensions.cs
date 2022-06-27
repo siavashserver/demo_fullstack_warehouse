bc3f551719201df358139b5aedcd8536b4a4da22
Siavash Eliasi <siavashserver@gmail.com>
2022-06-27 18:37:31 +0430

    Implemented Core service registration extensions.

---
+++ backend/Core/Extensions/CoreServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Extensions;
+
+public static class CoreServiceExtensions
+{
+    public static IServiceCollection AddCoreServices(this IServiceCollection services, IConfiguration config)
+    {
+        // Setup MediatR
+        services.AddMediatR(typeof(CoreServiceExtensions));
+
+        return services;
+    }
+}
