c6c49c6e1c359ada9c87a96423ac490f134976ca
Siavash Eliasi <siavashserver@gmail.com>
2022-06-27 18:13:50 +0430

    Implemented infra service registration extension.

---
+++ backend/Infra/Extensions/InfraServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Core.Interfaces;
+using Infra.DataAccess;
+using Infra.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.Extensions;
+
+public static class InfraServiceExtensions
+{
+    public static IServiceCollection AddInfraServices(this IServiceCollection services, IConfiguration config)
+    {
+        // Setup Repositories
+        services.AddScoped<ICustomerRepo, CustomerRepo>();
+        services.AddScoped<IProductRepo, ProductRepo>();
+        services.AddScoped<IOrderRepo, OrderRepo>();
+        services.AddScoped<ILineItemRepo, LineItemRepo>();
+
+        // Setup SQLite DataBase
+        services.AddDbContext<DataContext>(options =>
+        {
+            options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
