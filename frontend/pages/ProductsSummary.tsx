e51e67f12073bcfe69f0efbdbc9be7746ec8eeb8
Siavash Eliasi <siavashserver@gmail.com>
2022-07-02 21:49:36 +0430

    Initial Products Summary page implementation.

---
+++ frontend/pages/ProductsSummary.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import React from "react";
+
+const ProductsSummary: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Products Summary</title>
+      </Head>
+      <div>Products Summary</div>
+    </>
+  );
+};
+
+export default ProductsSummary;
