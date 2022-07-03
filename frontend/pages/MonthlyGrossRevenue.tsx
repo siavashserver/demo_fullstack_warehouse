e06844c9f68b7eea8d5bc82ddc64e67634d5a2f1
Siavash Eliasi <siavashserver@gmail.com>
2022-07-03 10:21:24 +0430

    Initial Monthly Revenue page implementation.

---
+++ frontend/pages/MonthlyGrossRevenue.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import React from "react";
+
+const MonthlyGrossRevenue: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Monthly Gross Revenue</title>
+      </Head>
+      <div>Monthly Gross Revenue</div>
+    </>
+  );
+};
+
+export default MonthlyGrossRevenue;
