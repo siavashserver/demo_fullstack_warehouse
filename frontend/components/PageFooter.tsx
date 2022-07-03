36add24d280a31a76690c4cb8f98ef4ccd468ff0
Siavash Eliasi <siavashserver@gmail.com>
2022-07-03 13:43:09 +0430

    Modified page footer.

--- frontend/components/PageFooter.tsx
+++ frontend/components/PageFooter.tsx
@@ -5,7 +5,7 @@ import { Layout } from "antd";
 const { Footer } = Layout;
 
 const PageFooter = () => {
-  return <Footer>PageFooter</Footer>;
+  return <Footer>Warehouse • Full Stack Demo</Footer>;
 };
 
 export default PageFooter;
